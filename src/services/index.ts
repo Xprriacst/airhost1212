fc080fe296dfdc52274b28342acc67d01749c358
Xprriacst <expertiaen5min@gmail.com>
2025-01-18 14:25:48 +0100

    fix: Ajout des exports manquants pour authService et authorizationService

--- src/services/index.ts
+++ src/services/index.ts
@@ -2,10 +2,14 @@ import { propertyService } from './airtable/propertyService';
 import { conversationService } from './airtable/conversationService';
 import { messageService } from './airtable/messageService';
 import { aiService } from './ai/aiService';
+import { authorizationService } from './authorizationService';
+import { authService } from './auth/authService';
 
 export {
   propertyService,
   conversationService,
   messageService,
-  aiService
+  aiService,
+  authorizationService,
+  authService
 };
