f1d93265c3c1b027cd90b3183c379a3e3f00c15f
Xprriacst <expertiaen5min@gmail.com>
2025-02-03 11:42:43 +0100

    fix: correction de l'export du service WhatsApp

--- src/services/whatsapp/index.ts
+++ src/services/whatsapp/index.ts
@@ -2,3 +2,10 @@ export * from './types';
 export * from './factory';
 export * from './makeService';
 export * from './officialService';
+
+import { WhatsAppServiceConfig } from './types';
+import { WhatsAppServiceFactory } from './factory';
+
+export const getWhatsAppService = (config: WhatsAppServiceConfig) => {
+  return WhatsAppServiceFactory.getInstance().getService(config);
+};
