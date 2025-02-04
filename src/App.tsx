8336f3f1aefce83e3962cd6a0c7d75c5e1a21f23
Xprriacst <expertiaen5min@gmail.com>
2025-02-04 15:55:08 +0100

    feat: ajout des flags de compatibilité React Router v7

--- src/App.tsx
+++ src/App.tsx
@@ -264,7 +264,12 @@ function App() {
   const { isAuthenticated } = useAuthStore();
 
   return (
-    <Router>
+    <Router
+      future={{
+        v7_startTransition: true,
+        v7_relativeSplatPath: true
+      }}
+    >
       <Routes>
         <Route
           path="/login"
