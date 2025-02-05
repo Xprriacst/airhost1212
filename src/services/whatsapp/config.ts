2e4afc06cdfb0ede677773c5663f7474b1f0b322
Xprriacst <expertiaen5min@gmail.com>
2025-02-05 17:22:23 +0100

    feat: ajout de la configuration WhatsApp avec le nouveau token

---
+++ src/services/whatsapp/config.ts
@@ -0,0 +1,10 @@
+import { WhatsAppConfig } from '../../types/whatsapp';
+
+export const whatsappConfig: WhatsAppConfig = {
+  provider: 'official',
+  appId: '1676211843267502',
+  accessToken: 'EAAX0gXt8e64BOzWTcXAJZBCKuwmpgVDnoynzX5QduPvlMF2580gcAnPPffpm0zzPl711o1YvB7YbKcoKOBYWvBjK5aRFgeZAWZCPanuXXG4QZBDUEbxe2ZA6ZAlhjmvBmB0P8aZByx1e5guR6kDf4YrX8MhF4DFjkeuGSTNEyEiZCPYyyvY5ewZB36pDZBYRThKZBdKcqgGj4dVdZCZBX8K7VJbqFleUP',
+  apiVersion: 'v21.0',
+  phoneNumberId: '477925252079395',
+  apiUrl: 'https://graph.facebook.com/v21.0'
+};
